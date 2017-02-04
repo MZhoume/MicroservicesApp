73dc8ee5642b4dc38a24e37a98e612a95bbd859f
Mingyang Zheng <Zmy-Apple@MingyangdeMacBook-Pro.local>
2017-02-04 00:09:29 -0500

    change the part that was deleted by mistake
    change the part that was deleted by mistake

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../User";
-
+import {Router} from "@angular/router";
+import {UserService} from "../user.service";
 @Component({
     selector: 'app-register',
     templateUrl: './register.component.html',
     styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-    model: User;
+    regUser: User;
+    message: string;
 
-    constructor() { }
+    constructor(
+        private userService: UserService,
+        private router: Router,
+    ) { }
 
     ngOnInit() {
-        this.model = new User();
+        this.regUser = new User();
+        // if usr log in, redirect to welcome page
+        if (this.userService.getUser() == undefined){
+            console.log("please register");
+        }else {
+            this.forward();
+        }
+        
     }
 
-    onSubmit() { }
+    forward() {
+        this.router.navigate(['/welcome']);
+    }
+
+    onSubmit() {
+        console.log("going to register");
+        this.message = 'Loading';
+        this.userService.registerUserRemote(this.regUser).then(response => {
+            if (response.flag == 'success') {
+                this.regUser = new User();
+                this.message ='register success';
+                console.log('register success');
+                this.forward();
+            } else {
+                console.log(response);
+                this.message = response.reason;
+                console.log('register fail');
+            }
+        });
+    }
+
 
 }
