46890507a03b37c23ca96f466024f34f91ef20f2
Mingyang Zheng <Zmy-Apple@MingyangdeMacBook-Pro.local>
2017-02-03 23:53:45 -0500

    modifiy
    modify

--- Frontend/src/app/register/register.component.ts
+++ Frontend/src/app/register/register.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {User} from "../User";
-import {Router} from "@angular/router";
-import {UserService} from "../user.service";
 
 @Component({
     selector: 'app-register',
@@ -9,43 +7,14 @@ import {UserService} from "../user.service";
     styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-    regUser: User;
-    message: string;
+    model: User;
 
-    constructor(
-        private userService: UserService,
-        private router: Router,
-    ) { }
+    constructor() { }
 
     ngOnInit() {
-        this.regUser = new User();
-        // if usr log in, redirect to welcome page
-        if (this.userService.getUser() == undefined){
-            console.log("please register");
-        }else {
-            this.forward();
-        }
+        this.model = new User();
     }
 
-    forward() {
-        this.router.navigate(['/welcome']);
-    }
-
-    onSubmit() {
-        console.log("going to register");
-        this.message = 'Loading';
-        this.userService.registerUserRemote(this.regUser).then(response => {
-            if (response.flag == 'success') {
-                this.regUser = new User();
-                this.message ='register success';
-                console.log('register success');
-                this.forward();
-            } else {
-                console.log(response);
-                this.message = response.reason;
-                console.log('register fail');
-            }
-        });
-    }
+    onSubmit() { }
 
 }
--- Frontend/src/app/sign-out/sign-out.component.ts
+++ Frontend/src/app/sign-out/sign-out.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../user.service";
 import {Router} from "@angular/router";
+import {User} from "../User";
 
 @Component({
     selector: 'app-sign-out',
@@ -8,7 +9,7 @@ import {Router} from "@angular/router";
     styleUrls: ['./sign-out.component.css']
 })
 export class SignOutComponent implements OnInit {
-
+    user: User;
     message: string;
 
     constructor(
@@ -18,6 +19,7 @@ export class SignOutComponent implements OnInit {
 
     ngOnInit() {
         this.message = "loading";
+        this.user = new User();
         // prevent unlog usr get in
         if (this.userService.getUser() == undefined){
             this.router.navigate(['/']);
@@ -30,6 +32,32 @@ export class SignOutComponent implements OnInit {
     signout(){
         this.userService.getOffUser();
         this.message ="Success log out"
+    }
+
+    onSubmit() {
+        console.log("going to log in");
+        this.message = 'Loading';
+        this.userService.loginUserRemote(this.user).then(response => {
+            if (response.flag == 'success') {
+                this.user = this.userService.getUser();
+                this.message ='login success';
+                console.log('login success');
+                this.forward();
+            } else {
+                console.log(response);
+                this.message = response.reason;
+                console.log('login fail');
+            }
+        });
+
+    }
+
+    forward() {
+        this.router.navigate(['/welcome']);
+    }
+
+    onClick() {
+        this.router.navigate(['/register']);
     }
 
 }
