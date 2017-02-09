63e801f876f470356d3fc12dc37cfe9fb19e34c6
M Zhou <contact@mzhou.me>
2017-02-09 01:53:08 -0500

    fix error so travis will passing

--- Backend/Test/ExampleService.Test/FunctionTest.cs
+++ Backend/Test/ExampleService.Test/FunctionTest.cs
@@ -14,7 +14,7 @@ namespace ExampleService.Test
             var context = new TestLambdaContext();
             var upperCase = function.FunctionHandler("hello world", context);
 
-            Assert.Equal("HELLO WORL", upperCase);
+            Assert.Equal("HELLO WORLD", upperCase);
         }
     }
 }
