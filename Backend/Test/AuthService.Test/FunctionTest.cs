5bbe524e398406f238dab56ef89808323894a51f
M Zhou <contact@mzhou.me>
2017-02-10 18:17:50 -0500

    split test into success and fail

--- Backend/Test/AuthService.Test/FunctionTest.cs
+++ Backend/Test/AuthService.Test/FunctionTest.cs
@@ -1,36 +1,40 @@
 namespace AuthService.Test
 {
-    using Amazon.Lambda.TestUtilities;
     using Amazon.Lambda.APIGatewayEvents;
+    using Amazon.Lambda.Core;
+    using Amazon.Lambda.TestUtilities;
     using AuthService;
     using Xunit;
 
     public class FunctionTest
     {
-        [Fact]
-        public void TestAuthFunction()
-        {
-            // Invoke the lambda function and confirm the string was upper cased.
-            var function = new Function();
-            var context = new TestLambdaContext();
+        private Function function = new Function();
+        private ILambdaContext context = new TestLambdaContext();
 
+        [Fact]
+        public void TestAuthFunctionSuccess()
+        {
             var validRequest = new APIGatewayCustomAuthorizerRequest()
             {
                 AuthorizationToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVc2VySWQiOjAsIkZpcnN0TmFtZSI6IkFkbWluIiwiTGFzdE5hbWUiOiJBZG1pbiJ9.xGO4pOrx1HWHoairSmtL556Bu2h6Sot402ySVZttgrM",
                 MethodArn = "arn:aws:execute-api:<regionId>:<accountId>:<apiId>/<stage>/<method>/<resourcePath>"
             };
 
-            var validPolicy = function.FunctionHandler(validRequest, context).PolicyDocument;
+            var validPolicy = this.function.FunctionHandler(validRequest, this.context).PolicyDocument;
             Assert.True(validPolicy.Statement[0].Action.Contains(CustomAuthorizerHelper.ComposeAction(Action.Invoke)));
             Assert.Equal(validPolicy.Statement[0].Effect, Effect.Allow);
+        }
 
+        [Fact]
+        public void TestAuthFunctionFail()
+        {
             var invalidRequest = new APIGatewayCustomAuthorizerRequest()
             {
                 AuthorizationToken = "invalid",
                 MethodArn = "arn:aws:execute-api:<regionId>:<accountId>:<apiId>/<stage>/<method>/<resourcePath>"
             };
 
-            var invalidPolicy = function.FunctionHandler(invalidRequest, context).PolicyDocument;
+            var invalidPolicy = this.function.FunctionHandler(invalidRequest, this.context).PolicyDocument;
             Assert.True(invalidPolicy.Statement[0].Action.Contains(CustomAuthorizerHelper.ComposeAction(Action.All)));
             Assert.Equal(invalidPolicy.Statement[0].Effect, Effect.Deny);
         }
