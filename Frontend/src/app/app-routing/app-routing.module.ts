dfcffc5f82b268c573046268107dacdd74b93f9d
huafeng <hs2917@columbia.edu>
2017-02-10 16:58:21 -0500

    change structure

--- Frontend/src/app/app-routing/app-routing.module.ts
+++ Frontend/src/app/app-routing/app-routing.module.ts
@@ -5,13 +5,16 @@ import {UserLoginComponent} from "../user-login/user-login.component";
 import {WelcomeComponent} from "../welcome/welcome.component";
 import {SignOutComponent} from "../sign-out/sign-out.component";
 import {RegisterComponent} from "../register/register.component";
+import {ShoppingComponent} from "../shopping/shopping.component";
 
 
 const routes: Routes = [
-    { path: '',  component: UserLoginComponent },
+    { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+    { path: 'login',  component: UserLoginComponent },
     { path: 'welcome',  component: WelcomeComponent },
     { path: 'signout',  component: SignOutComponent },
     { path: 'register',  component: RegisterComponent },
+    { path: 'shopping',  component: ShoppingComponent },
 ];
 
 
