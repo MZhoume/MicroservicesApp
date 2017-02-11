3165e9795adf1d8bb8b55e669d32b60a30851763
M Zhou <contact@mzhou.me>
2017-02-10 21:32:15 -0500

    add lambda exception

---
+++ Backend/Source/Static/LambdaException.cs
@@ -0,0 +1,20 @@
+namespace Static
+{
+    using System;
+
+    /// <summary>
+    /// Exception class for lambda contains http status code and message
+    /// </summary>
+    public class LambdaException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LambdaException"/> class
+        /// </summary>
+        /// <param name="code"> Http status code </param>
+        /// <param name="message"> Error message </param>
+        public LambdaException(HttpCode code, string message)
+            : base($"[{code}] | {message}")
+        {
+        }
+    }
+}
