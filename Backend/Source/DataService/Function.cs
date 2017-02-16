f286acd97ab677d234eab6f08b311d64c32b1e56
M Zhou <contact@mzhou.me>
2017-02-16 11:09:27 -0500

    add Orchestrator, EmailService, UserService, UserInfoService, OrderService, PaymentService and DataService

---
+++ Backend/Source/DataService/Function.cs
@@ -0,0 +1,22 @@
+namespace DataService
+{
+    using Amazon.Lambda.Core;
+
+    /// <summary>
+    /// Lambda function entry class
+    /// </summary>
+    public class Function
+    {
+        /// <summary>
+        /// Example lambda function handler
+        /// </summary>
+        /// <param name="input"> Input for lambda handler </param>
+        /// <param name="context"> Context info for lambda handler </param>
+        /// <returns> Value send to clients </returns>
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
