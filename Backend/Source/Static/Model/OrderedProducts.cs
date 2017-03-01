550a00b79b1c21092d7f62511b158e94777d8beb
zhengzhi <xdzhizheng@yahoo.com>
2017-02-28 19:01:43 -0500

    Add model OrderedProducts

---
+++ Backend/Source/Static/Model/OrderedProducts.cs
@@ -0,0 +1,31 @@
+namespace Static.Model
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Define OrderedProducts class
+    /// </summary>
+    public class OrderedProducts
+    {
+        /// <summary>
+        /// Gets or sets order id
+        /// </summary>
+        /// <returns>Return order id</returns>
+        [Key]
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// Gets or sets product id
+        /// </summary>
+        /// <returns>Return product id</returns>
+        [Key]
+        public int ProductId { get; set; }
+
+        /// <summary>
+        /// Gets or sets product amount
+        /// </summary>
+        /// <returns>Return amount</returns>
+        [Required]
+        public int Amount { get; set; }
+    }
+}
