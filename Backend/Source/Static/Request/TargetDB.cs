c5ccfea2864f2f3ae5032acc308b1597ff730fdf
zhengzhi <xdzhizheng@yahoo.com>
2017-02-28 19:22:57 -0500

    add target db name class

---
+++ Backend/Source/Static/Request/TargetDB.cs
@@ -0,0 +1,19 @@
+namespace Static.Request
+{
+    /// <summary>
+    /// Type for Target Database
+    /// </summary>
+    public enum TargetDB
+    {
+#pragma warning disable CS1591
+#pragma warning disable SA1602
+        Address,
+        Order,
+        OrderedProducts,
+        Payment,
+        Product,
+        User
+#pragma warning disable CS1591
+#pragma warning disable SA1602
+    }
+}
