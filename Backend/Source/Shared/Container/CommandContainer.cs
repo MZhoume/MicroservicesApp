1cf5d7af52fca1dd7b115ed533026b924133dcf8
M Zhou <contact@mzhou.me>
2017-03-20 16:46:12 -0400

    add support for requirements in the CommandContainer

--- Backend/Source/Shared/Container/CommandContainer.cs
+++ Backend/Source/Shared/Container/CommandContainer.cs
@@ -4,31 +4,76 @@ namespace Shared.Container
     using System.Collections.Generic;
     using Shared.Interface;
     using Shared.Request;
+    using SimpleInjector;
 
     /// <summary>
     /// Container service for commands
     /// </summary>
     public class CommandContainer
     {
-        private readonly Dictionary<Operation, Type> container = new Dictionary<Operation, Type>();
+        private readonly Dictionary<Operation, Type> commands = new Dictionary<Operation, Type>();
+        private readonly Container container = new Container();
 
         /// <summary>
         /// Gets the registered command with given key
         /// </summary>
         /// <param name="operation"> The Operation to register </param>
-        public ICommand this[Operation operation] => (ICommand)Activator.CreateInstance(this.container[operation]);
+        public ICommand this[Operation operation] => this.container.GetInstance(this.commands[operation]) as ICommand;
 
         /// <summary>
         /// Register a command with the key
         /// </summary>
         /// <param name="operation"> The Operation to register </param>
         /// <typeparam name="TCommand"> The type of the command </typeparam>
-        /// <returns> This for chining call </returns>
+        /// <returns> This for chaining call </returns>
         public CommandContainer Register<TCommand>(Operation operation)
         where TCommand : ICommand
         {
-            this.container.Add(operation, typeof(TCommand));
+            if (this.commands.ContainsKey(operation))
+            {
+                throw new ArgumentException($"{Enum.GetName(typeof(Operation), operation)} already registered.");
+            }
 
+            this.commands.Add(operation, typeof(TCommand));
+            return this;
+        }
+
+        /// <summary>
+        /// Register a requirement for the commands
+        /// </summary>
+        /// <typeparam name="TConcrete"> The type of the instance </typeparam>
+        /// <returns> This for chaining call</returns>
+        public CommandContainer RegisterRequirement<TConcrete>()
+        where TConcrete : class
+        {
+            this.container.Register<TConcrete>();
+            return this;
+        }
+
+        /// <summary>
+        /// Register a requirement for the commands
+        /// </summary>
+        /// <typeparam name="TInterface"> The type of the interface </typeparam>
+        /// <typeparam name="TConcrete"> The type of the instance </typeparam>
+        /// <returns> This for chaining call</returns>
+        public CommandContainer RegisterRequirement<TInterface, TConcrete>()
+        where TInterface : class
+        where TConcrete : class, TInterface
+        {
+            this.container.Register<TInterface, TConcrete>();
+            return this;
+        }
+
+        /// <summary>
+        /// Register a requirement for the commands
+        /// </summary>
+        /// <param name="func"> The func that returns the instance </param>
+        /// <typeparam name="TType"> The type of the interface </typeparam>
+        /// <returns> This for chaining call</returns>
+        public CommandContainer RegisterRequirement<TType>(Func<TType> func)
+        where TType : class
+        {
+            this.container.Register<TType>(func);
             return this;
         }
     }
--- Backend/Test/Shared.Test/CommandContainerTest.cs
+++ Backend/Test/Shared.Test/CommandContainerTest.cs
@@ -25,5 +25,78 @@ namespace Shared.Test
 
             Assert.Equal(typeof(TestCommand), container[Operation.Create].GetType());
         }
+
+        private class TestCommand2 : ICommand
+        {
+            private ICommand command;
+
+            public TestCommand2(ICommand command)
+            {
+                this.command = command;
+            }
+
+            public ICommand Test()
+            {
+                return this.command;
+            }
+
+            public Response Invoke(Request request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        private class TestCommand3 : ICommand
+        {
+            private ICommand command;
+
+            public TestCommand3(TestCommand command)
+            {
+                this.command = command;
+            }
+
+            public ICommand Test()
+            {
+                return this.command;
+            }
+
+            public Response Invoke(Request request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [Fact]
+        public void ContainerShouldWorkWithConcreteRequirement()
+        {
+            var container = new CommandContainer();
+            container.RegisterRequirement<TestCommand>()
+                     .Register<TestCommand3>(Operation.Create);
+
+            Assert.Equal(typeof(TestCommand3), container[Operation.Create].GetType());
+            Assert.Equal(typeof(TestCommand), ((TestCommand3)container[Operation.Create]).Test().GetType());
+        }
+
+        [Fact]
+        public void ContainerShouldWorkWithInterfaceRequirement()
+        {
+            var container = new CommandContainer();
+            container.RegisterRequirement<ICommand, TestCommand>()
+                     .Register<TestCommand2>(Operation.Create);
+
+            Assert.Equal(typeof(TestCommand2), container[Operation.Create].GetType());
+            Assert.Equal(typeof(TestCommand), ((TestCommand2)container[Operation.Create]).Test().GetType());
+        }
+
+        [Fact]
+        public void ContainerShouldWorkWithRequirementConcrete()
+        {
+            var container = new CommandContainer();
+            container.RegisterRequirement<ICommand>(() => new TestCommand())
+                     .Register<TestCommand2>(Operation.Create);
+
+            Assert.Equal(typeof(TestCommand2), container[Operation.Create].GetType());
+            Assert.Equal(typeof(TestCommand), ((TestCommand2)container[Operation.Create]).Test().GetType());
+        }
     }
 }
