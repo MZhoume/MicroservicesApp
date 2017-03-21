e2c5e8eeebee309d4064f8d6b5d1b1d722de92cb
M Zhou <contact@mzhou.me>
2017-03-21 16:22:43 -0400

    add debugging test

--- Backend/Test/UserService.Test/FunctionTest.cs
+++ Backend/Test/UserService.Test/FunctionTest.cs
@@ -1,20 +1,39 @@
 namespace UserService.Test
 {
     using Amazon.Lambda.TestUtilities;
+    using Shared.Request;
     using UserService;
     using Xunit;
 
     public class FunctionTest
     {
-        // [Fact]
-        // public void TestToUpperFunction()
-        // {
-        //     // Invoke the lambda function and confirm the string was upper cased.
-        //     var function = new Function();
-        //     var context = new TestLambdaContext();
-        //     var upperCase = function.FunctionHandler("hello world", context);
+        [Fact]
+        public void TestFunction()
+        {
+            var function = new Function();
+            var context = new TestLambdaContext();
 
-        //     Assert.Equal("HELLO WORLD", upperCase);
-        // }
+            var request = new Request()
+            {
+                AuthToken = "Token",
+                Operation = Operation.Read,
+                Payload = null,
+                PagingInfo = new PagingInfo()
+                {
+                    Start = 0,
+                    Count = 10
+                },
+                SearchTerm = new[] {
+                    new SearchTerm()
+                    {
+                        Field = "Email",
+                        Operator = SearchOperator.LIKE,
+                        Value = "test"
+                    }
+                }
+            };
+
+            var response = function.FunctionHandler(request, context);
+        }
     }
 }
