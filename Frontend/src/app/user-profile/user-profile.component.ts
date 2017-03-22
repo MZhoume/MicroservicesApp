883ecc9766bc1941e37e2383b410bf8bbd531d1d
Mingyang Zheng <Zmy-Apple@dyn-160-39-147-145.dyn.columbia.edu>
2017-03-22 14:34:40 -0400

    add logic

--- Frontend/src/app/user-profile/user-profile.component.ts
+++ Frontend/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from "../User";
+import {Router} from "@angular/router";
+import {UserService} from "../user.service";
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +11,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
-  constructor() { }
+    regUser: User;
+    message: string;
+
+    constructor(
+        private userService: UserService,
+        private router: Router,
+    ) { }
 
   ngOnInit() {
+      this.regUser = new User();
+      // if usr log in, redirect to welcome page
+      if (this.userService.getUser() == undefined){
+          this.forward('/welcome');
+      }else {
+          console.log("please modify");
+      }
   }
+    forward(dest:string) {
+        this.router.navigate([dest]);
+    }
+
+    async onSubmit() : Promise<any> {
+        console.log("going to register");
+        this.message = 'Loading';
+        try {
+            let resigterResult = await this.userService.registerUserRemote(this.regUser);
+            console.log(resigterResult);
+            if (resigterResult.result  === 'success') {
+                this.regUser = new User();
+                this.message ='register success';
+                console.log('register success');
+                this.forward('/login');
+            } else {
+                console.log(resigterResult);
+                //this.message = response.reason;
+                console.log('register fail');
+            }
+        } catch (ex) {
+            console.error('An error occurred', ex);
+        }
+    }
+
+
 
 }
